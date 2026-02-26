86279d068a8a1ae974b037f52e2cf4d789e6371b
mopornvisa <pornvisa.mo@gmail.com>
2026-02-26 10:22:12 +0700

    add-26-02-69

--- resources/views/household_64.blade.php
+++ resources/views/household_64.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
+  
 
   @php
     $survey_year = $survey_year ?? request('survey_year','');
